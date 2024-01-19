187d41412d8087f94e904c0b0e80e74349d64694
Arsh <pawar.arsh1333@gmail.com>
2024-01-19 21:44:59 +0530

    sub tag

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
     <div className="hero">
       <div className="hero-content">
         <h1 className="hero-h">The Growth Handbook</h1>
-
+        <h2 className="hero-h">For the Readers , By a Reader</h2>
         <p className="hero-p">
           Welcome to a world of words, where stories come to life and adventures
           unfold on every page. Immerse yourself in the magic of literature,
@@ -25,7 +25,6 @@ export default function Hero() {
           <button className="hero-btn-nf">Nonfiction</button>
         </Link>
       </div>
-
       <img src={heroImg} className="hero-img"></img>
     </div>
   );
